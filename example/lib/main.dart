88b29160d0881af24a799acdb014c79d8d0b3c9d
HeavenOSK <ryunosukeheaven.ba@gmail.com>
2019-10-21 19:31:18 +0900

    create Project

---
+++ example/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:animated_opacity_button/animated_opacity_button.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AnimatedOpacityButton Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'AnimatedOpacityButton Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedOpacityButton(
+                  padding: EdgeInsets.all(8),
+                  child: Icon(Icons.add),
+                  onTap: _incrementCounter,
+                ),
+                SizedBox(width: 40),
+                AnimatedOpacityButton(
+                  padding: EdgeInsets.all(8),
+                  child: Text('ADD'),
+                  onTap: _incrementCounter,
+                ),
+                SizedBox(width: 40),
+                AnimatedOpacityButton(
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.add),
+                      SizedBox(height: 8),
+                      Text('ADD'),
+                    ],
+                  ),
+                  onTap: _incrementCounter,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/animated_opacity_button.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class AnimatedOpacityButton extends StatefulWidget {
+  AnimatedOpacityButton({
+    Key key,
+    this.alignment,
+    this.padding,
+    double width,
+    double height,
+    BoxConstraints constraints,
+    this.canTap = true,
+    this.onTap,
+    Duration duration,
+    this.minimumOpacity = 0.4,
+    @required this.child,
+  })  : assert(padding == null || padding.isNonNegative),
+        assert(constraints == null || constraints.debugAssertIsValid()),
+        constraints = (width != null || height != null)
+            ? constraints?.tighten(width: width, height: height) ??
+                BoxConstraints.tightFor(width: width, height: height)
+            : constraints,
+        duration = duration ?? Duration(milliseconds: 30),
+        super(key: key);
+
+  final Widget child;
+  final bool canTap;
+  final VoidCallback onTap;
+  final EdgeInsetsGeometry padding;
+  final AlignmentGeometry alignment;
+  final BoxConstraints constraints;
+  final double minimumOpacity;
+  final Duration duration;
+
+  @override
+  _AnimatedOpacityButtonState createState() => _AnimatedOpacityButtonState();
+}
+
+class _AnimatedOpacityButtonState extends State<AnimatedOpacityButton> {
+  bool _canTap;
+  bool _isPressing = false;
+
+  @override
+  void initState() {
+    _canTap = widget.canTap;
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(AnimatedOpacityButton oldWidget) {
+    _canTap = widget.canTap;
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AbsorbPointer(
+      absorbing: !_canTap,
+      child: GestureDetector(
+        onTap: widget.onTap,
+        onTapUp: (_) => setState(() {
+          _isPressing = false;
+        }),
+        onTapCancel: () => setState(() {
+          _isPressing = false;
+        }),
+        onTapDown: (_) => setState(() {
+          _isPressing = true;
+        }),
+        child: AnimatedOpacity(
+          opacity: _isPressing ? widget.minimumOpacity : 1.0,
+          duration: widget.duration,
+          child: Container(
+            constraints: widget.constraints,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+            ),
+            padding: widget.padding,
+            alignment: widget.alignment,
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
